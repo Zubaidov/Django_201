a1e623695c766070707e18be6a6da9b34c2bbe32
Zubaidov <shohruz.zubaidov@gmail.com>
2023-05-10 12:50:50 +0400

    templates issue is fixed

--- til/urls.py
+++ til/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from mainapp import urls as mainapp_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(mainapp_urls, namespace="mainapp")),
 ]
